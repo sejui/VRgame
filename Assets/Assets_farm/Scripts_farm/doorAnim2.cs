cb5d51ad54f09bba5289722f61cfb7badf49e001
Unknown <sejui@hotmail.com>
2017-06-20 16:16:45 -0700

    merged scenes

---
+++ Assets/Assets_farm/Scripts_farm/doorAnim2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorAnim2 : MonoBehaviour {
+	public Animator anim;
+//	public AudioSource windchime;
+	void Start(){
+		anim = GetComponent<Animator>();
+//		windchime = GetComponent<AudioSource> ();
+	}
+	void OnTriggerEnter(Collider col){
+		Debug.Log ("trigger mill animation");
+//		windchime.Play ();
+		if (col.gameObject.tag == "Player") {
+			anim.Play ("doorOpen2");
+
+		}
+	}
+}
