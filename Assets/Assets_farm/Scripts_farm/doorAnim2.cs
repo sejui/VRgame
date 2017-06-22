72787e06b5faf74adf1181c7816dcc8fa81a3623
Unknown <sejui@hotmail.com>
2017-06-22 14:11:32 -0700

    ending

--- Assets/Assets_farm/Scripts_farm/doorAnim2.cs
+++ Assets/Assets_farm/Scripts_farm/doorAnim2.cs
@@ -14,15 +14,14 @@ public class doorAnim2 : MonoBehaviour {
 	void OnTriggerEnter(Collider col){
 
 		if (col.gameObject.tag == "Player") {
-			
+			anim.Play ("doorOpen2");
 		}
 	}
 
-	void OnTriggerStay(Collider col){
+	void OnTriggerExit(Collider col){
 		if (col.gameObject.tag == "Player") {
-			anim.Play ("doorOpen2");
-			countdown --;
-			Debug.Log (countdown);
+			
+			Application.LoadLevel(0);
 		}
 
 	}
@@ -30,7 +29,7 @@ public class doorAnim2 : MonoBehaviour {
 	void Update(){
 		if (countdown <= 0) {
 			//fade the camera, load the starting screen 
-			Application.LoadLevel(0);
+
 		}
 
 	}
