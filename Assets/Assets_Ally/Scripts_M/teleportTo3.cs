0fc1274bfb59f22fa4093caa065047a6ecf420c6
maureensmr <maureensmmr@gmail.com>
2017-06-21 22:35:44 -0700

    added start scene + alley scene updated

--- Assets/Assets_Ally/Scripts_M/teleportTo3.cs
+++ Assets/Assets_Ally/Scripts_M/teleportTo3.cs
@@ -9,6 +9,8 @@ public class teleportTo3 : MonoBehaviour
     {
         Debug.Log("teleport begins");
         //		other.transform.position = new Vector3(29,1,22);
+		if (other.gameObject.tag == "Player") { 
         Application.LoadLevel(3);   //you can also put in the scene number instead of the name: Application.LoadLevel(1);
+		}
     }
 }
